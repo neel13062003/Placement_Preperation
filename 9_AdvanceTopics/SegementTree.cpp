dc757e990bd4be7f2677f6e4e76acd2f1cc575e4
Neel Soni <neelnsoni13@gmail.com>
2023-07-11 09:44:59 +0530

    Segement Tree

---
+++ 9_AdvanceTopics/SegementTree.cpp
@@ -0,0 +1,6 @@
+/*Range Based Query - When You Got TLE solving simple problem might be solved using this concept*/
+/*Lazy Propogation, Fenwick Tree*/
+//If You have enough time & you completed almost all of dsa concepts then & only learn this otherwise you will waste your time.
+
+First = "https://www.youtube.com/live/NEG-SoyigGE?feature=share"
+Second =  "https://www.youtube.com/live/rXnXRU8yMF0?feature=share"
