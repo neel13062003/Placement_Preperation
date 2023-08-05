635dcbaf26cf1609582af2c08cb267735d493e8e
Neel Soni <neelnsoni13@gmail.com>
2023-08-05 07:53:09 +0530

    Java

---
+++ Java/Array.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3}; // Corrected the array initialization
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]); // Corrected "system" to "System" for printing
+        }
+    }
+}
